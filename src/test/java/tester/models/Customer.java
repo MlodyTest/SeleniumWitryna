54c1c9d45b931b9c549151c6e905d2e0d2415c19
Slawomir Pasieczny <spasieczny@inteca.pl>
2022-09-02 22:06:11 +0200

    Zmiana

--- src/test/java/tester/models/Customer.java
+++ src/test/java/tester/models/Customer.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class Customer {
 
-    private String firstName = "MichalNie";
+    private String firstName = "MichalNie2";
     private String lastName = "Pasieczny";
     private String companyName = "TestCompany";
     private String countryName = "Poland";
